835dd4960025132d232c227191adbb6cb07f4165
Oluwatemmy <oluwaseyitemitope456@gmail.com>
2025-05-06 13:47:00 +0100

    Update database settings to use PostgreSQL; modify requirements for psycopg2

--- backend/settings.py
+++ backend/settings.py
@@ -89,9 +89,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_db',
+        'USER': 'blog_user',
+        'PASSWORD': 'blog_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
